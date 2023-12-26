f74794e165cd65db9b8172570141f2634552e902
Nicky N <pnqcad@gmail.com>
2023-12-26 17:54:17 -0500

    Fix vm bootloader

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -6,10 +6,10 @@ inputs@{ config, pkgs, ... }:
       ./hardware-configuration.nix
     ];
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
